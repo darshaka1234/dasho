a0c8d039f82b8b552e6ee303c29a8b99c2fade27
darshaka1234 <darshakasrimal1234@gmail.com>
2023-09-03 21:32:24 +0530

    sql sever connected

---
+++ dasho/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using dasho.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dasho.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+     
+    }
+}
---
+++ dasho/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dasho.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
